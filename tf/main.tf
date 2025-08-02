92a5a5fda382734931ac8590f392f87da79daac8
olamide kosile <olamidekosile@gmail.com>
2025-08-02 20:39:15 +0100

    commit

--- tf/main.tf
+++ tf/main.tf
@@ -44,14 +44,16 @@ module "vpc" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 19.0"
+  version = "~> 18.0"
 
   cluster_name    = var.cluster_name
   cluster_version = "1.28"
 
-  vpc_id                         = module.vpc.vpc_id
-  subnet_ids                     = module.vpc.private_subnets
-  cluster_endpoint_public_access = true
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
 
   eks_managed_node_groups = {
     main = {
